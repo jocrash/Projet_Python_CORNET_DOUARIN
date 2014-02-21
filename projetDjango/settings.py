d29a85ca1200ec0f583373567c883f7f8b8fca56
jocrash <joel.cornet@esih.edu>
2014-02-21 09:03:17 -0500

    my django app

--- projetDjango/settings.py
+++ projetDjango/settings.py
@@ -93,8 +93,8 @@ TEMPLATE_DIRS = (
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
